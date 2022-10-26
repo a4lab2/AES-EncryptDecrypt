e34c174a016305a22ef41402e8e6265d768d362c
a4lab2 <a4lab2@gmail.com>
2022-10-26 18:09:13 +0100

    encrypt

--- main.go
+++ main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"crypto/aes"
+	"crypto/cipher"
 	"crypto/rand"
 	"encoding/hex"
+	"io"
 )
 
-func generateKey() string {
+// String version of the key
+func generateKey_String() string {
 	keyB := make([]byte, 32)
 	_, err := rand.Read(keyB)
 	if err != nil {
@@ -14,6 +18,45 @@ func generateKey() string {
 	key := hex.EncodeToString(keyB)
 	return key
 }
+
+// Overkill not really neccesary
+// func convertKeyToByte() {
+
+// }
+
+func encryptS(stringToEncrypt string) []byte {
+	keyString := generateKey_String()
+	key, _ := hex.DecodeString(keyString)
+	plaintext := []byte(stringToEncrypt)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+
+	}
+
+	aesGCM, err := cipher.NewGCM(block)
+	if err != nil {
+		panic(err)
+	}
+	nonce := make([]byte, aesGCM.NonceSize())
+
+	_, err = io.ReadFull(rand.Reader, nonce)
+	if err != nil {
+		panic(err)
+	}
+
+	encryptextText := aesGCM.Seal(nonce, nonce, plaintext, nil)
+
+	return encryptextText
+
+}
+
+// decrypt
+func decrypt(cipherText []byte) {
+
+}
+
 func main() {
 
 }
